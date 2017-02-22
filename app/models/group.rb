cfe842e4ee6bc00b3106de25c5997ce205a7bf1c
chouda <yujiankea@gmail.com>
2017-02-21 22:19:18 -0800

    join or quit group

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,8 +6,5 @@ belongs_to :user
   has_many :group_relationships
     has_many :members, through: :group_relationships, source: :user
 
-    def is_member_of?(group)
-        participated_groups.include?(group)
-      end
-
+    
 end
